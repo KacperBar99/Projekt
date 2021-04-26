e6a6667aa7aebf4657e9da6d0a6ee09603f94f22
Flamenco <konradstep23@gmail.com>
2021-04-26 15:13:02 +0200

    Okna dialogowe zapis i otwieranie poziomów
    Wczytywanie poziomów przy starcie edytora oraz zapisywanie przy kliknięciu escape do wybranej ścieżki z explorera.

--- Edytor/Platformer/src/Platformer/GamePanel.java
+++ Edytor/Platformer/src/Platformer/GamePanel.java
@@ -1,10 +1,12 @@
 package Platformer;
 
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
+import java.util.Timer;
 
 public class GamePanel extends javax.swing.JPanel implements ActionListener {
 
@@ -15,32 +17,38 @@ public class GamePanel extends javax.swing.JPanel implements ActionListener {
     ArrayList <WallB> wallsB = new ArrayList<>();
     ArrayList <KillBox> boxes = new ArrayList<>();
 
-    public GamePanel()
-    {
-        try {
-            File myObj = new File("level.txt");
-            Scanner myReader = new Scanner(myObj);
-            while (myReader.hasNextLine()) {
-                String data = myReader.nextLine();
+    public GamePanel() {
+        String username = System.getProperty("user.name");
+        JFileChooser fs = new JFileChooser(new File("C:\\Users\\" + username + "\\OneDrive\\Pulpit"));
 
-                switch (Integer.valueOf(data))
-                {
-                    case 0:
-                        walls.add(new Wall(Integer.valueOf(myReader.nextLine()),Integer.valueOf(myReader.nextLine())));
-                        break;
-                    case 1:
-                        wallsB.add(new WallB(Integer.valueOf(myReader.nextLine()),Integer.valueOf(myReader.nextLine())));
-                        break;
-                    case 2:
-                        changers.add(new Gravity_Changer(Integer.valueOf(myReader.nextLine()),Integer.valueOf(myReader.nextLine())));
-                        break;
+        fs.setDialogTitle("Otwórz...");
+        int result = fs.showOpenDialog(null);
+        if (result == JFileChooser.APPROVE_OPTION) {
+            try {
+                File myObj = new File(String.valueOf(fs.getSelectedFile()));
+                Scanner myReader = new Scanner(myObj);
+                while (myReader.hasNextLine()) {
+                    String data = myReader.nextLine();
+
+                    switch (Integer.valueOf(data)) {
+                        case 0:
+                            walls.add(new Wall(Integer.valueOf(myReader.nextLine()), Integer.valueOf(myReader.nextLine())));
+                            break;
+                        case 1:
+                            wallsB.add(new WallB(Integer.valueOf(myReader.nextLine()), Integer.valueOf(myReader.nextLine())));
+                            break;
+                        case 2:
+                            changers.add(new Gravity_Changer(Integer.valueOf(myReader.nextLine()), Integer.valueOf(myReader.nextLine())));
+                            break;
+                    }
                 }
+                myReader.close();
+            } catch (FileNotFoundException e) {
+                System.out.println("An error occurred.");
+                e.printStackTrace();
             }
-            myReader.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
         }
+
 
         cursor = new Cursor(400,300,this);
         gameTimer = new Timer();
@@ -58,8 +66,6 @@ public class GamePanel extends javax.swing.JPanel implements ActionListener {
 
             }
         });
-
-
 
         gameTimer.schedule(new TimerTask(){
 
